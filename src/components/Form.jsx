28c66f74c93be0b25d15f44cbd516241913ee127
David <david.ezequiel.perez@gmail.com>
2023-08-28 14:39:38 -0300

    Starting with the form, use of the useNavigate()  hook.

---
+++ src/components/Form.jsx
@@ -0,0 +1,78 @@
+const Form = ({client}) => {
+    return (
+        <>
+            <div className="mb-4">
+                <label
+                    className="text-gray-800"
+                    htmlFor="name"
+                >Name:</label>
+                <input 
+                    id="name"
+                    type="text"
+                    className="mt-2 block w-full p-3 bg-gray-50"
+                    placeholder="Name of the Client"
+                    name="name"
+                />
+            </div>
+
+            <div className="mb-4">
+                <label
+                    className="text-gray-800"
+                    htmlFor="company"
+                >Company:</label>
+                <input 
+                    id="company"
+                    type="text"
+                    className="mt-2 block w-full p-3 bg-gray-50"
+                    placeholder="Client's company."
+                    name="company"
+                />
+            </div>
+
+            <div className="mb-4">
+                <label
+                    className="text-gray-800"
+                    htmlFor="email"
+                >E-mail:</label>
+                <input 
+                    id="email"
+                    type="email"
+                    className="mt-2 block w-full p-3 bg-gray-50"
+                    placeholder="Client's email"
+                    name="email"
+                />
+            </div>
+
+            <div className="mb-4">
+                <label
+                    className="text-gray-800"
+                    htmlFor="phone"
+                >Phone:</label>
+                <input 
+                    id="phone"
+                    type="tel"
+                    className="mt-2 block w-full p-3 bg-gray-50"
+                    placeholder="Client's phone"
+                    name="phone"
+                />
+            </div>
+
+            <div className="mb-4">
+                <label
+                    className="text-gray-800"
+                    htmlFor="notes"
+                >Notes:</label>
+                <textarea
+                    as="textarea"
+                    id="notes"
+                    type="text"
+                    className="mt-2 block w-full p-3 bg-gray-50 h-40 align-self"
+                    placeholder="Client's notes"
+                    name="notes"
+                />
+            </div>
+        </>
+    )
+}
+
+export default Form
