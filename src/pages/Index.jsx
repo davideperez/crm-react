6e6fcbdd5f08c199123812a2aa72d6330d77d82b
David <david.ezequiel.perez@gmail.com>
2023-08-28 12:38:49 -0300

    Obtaining data from loader  with useLoaderData()

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,54 @@
+import { useLoaderData } from "react-router-dom"
+
 export const loader = () => {
   
-  return 'From Loader'
+  const clients = [
+    {
+        id: 1,
+        nombre: 'Juan',
+        telefono: 102013313,
+        email: "juan@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 2,
+        nombre: 'Karen',
+        telefono: 138198313,
+        email: "karen@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 3,
+        nombre: 'Josue',
+        telefono: 31983913,
+        email: "josue@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 4,
+        nombre: 'Miguel',
+        telefono: 319381983,
+        email: "miguel@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 5,
+        nombre: 'Pedro',
+        telefono: 1398198938,
+        email: "pedro@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+];
+
+  return clients
 }
 
 
 const Index = () => {
+
+  const data = useLoaderData()
+
+  console.log(data)
 
   return (
     <>
